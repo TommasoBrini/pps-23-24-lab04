3a183bb9fe24ce01ecd69f6cc5ef31e5e8f86f5d
Tommaso <tommaso.brini@studio.unibo.it>
2024-03-25 11:55:01 +0100

    implement given string

--- src/main/scala/tasks/typeclasses/Ex4Summables.scala
+++ src/main/scala/tasks/typeclasses/Ex4Summables.scala
@@ -31,14 +31,17 @@ object Ex4Summables:
   // write givens for Summable[Double] and Summable[String]
   given Summable[Double] with
     def sum(a1: Double, a2: Double): Double = a1 + a2
-    def zero: Double = 0.toDouble
+    def zero: Double = 0.0
+  
+  given Summable[String] with
+    def sum(a1: String, a2: String): String = "" + a1 + "" + a2
+    def zero: String = "" 
+
 
   @main def trySummables =
     val si = Cons(10, Cons(20, Cons(30, Nil())))  
     println:
       sumAllInt(si) // 60
-
-    // uncomment from here   
 
     println:
       sumAll(si) // 60
@@ -47,9 +50,7 @@ object Ex4Summables:
     println:
       sumAll(sd) // 60.0
 
-    /*
     val ss = Cons("10", Cons("20", Cons("30", Nil())))  
     println:
       sumAll(ss) // "102030"
-  */
 
