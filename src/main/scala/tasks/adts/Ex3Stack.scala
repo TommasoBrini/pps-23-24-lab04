36f0f183f21a3e3981eff37bf885ee2eb8435aea
Tommaso <tommaso.brini@studio.unibo.it>
2024-03-25 13:46:47 +0100

    implement empty ex3

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -22,9 +22,13 @@ object Ex3Stacks:
       def asSequence(): Sequence[A]
   
   object StackImpl extends StackADT:
-    type Stack[A] = Nothing
-    def empty[A]: Stack[A] = ???
+    import Sequence.*
+    type Stack[A] = Sequence[A]
+    def empty[A]: Stack[A] = Nil()
     extension [A](stack: Stack[A])
-      def push(a: A): Stack[A] = ???
+      def push(a: A): Stack[A] = stack match
+        case Cons(h, t) => Cons(h, t.push(a))
+        case Nil() => Nil() 
+      
       def pop(a: A): Optional[(A, Stack[A])] = ???
-      def asSequence(): Sequence[A] = ???
+      def asSequence(): Sequence[A] = stack
