2b8d69cc9f8f51dcfe410dc5f067c9261c61dea5
Tommaso <tommaso.brini@studio.unibo.it>
2024-03-26 13:06:00 +0100

    test create

--- src/test/scala/tasks/adts/SchoolModelTest.scala
+++ src/test/scala/tasks/adts/SchoolModelTest.scala
@@ -11,7 +11,16 @@ class SchoolModelTest{
     val bSchool: SchoolModule = basicSchool
     import bSchool.* 
 
+    val pps: Course = course("PPS")
+    val pcd: Course = course("PCD")
+    val lcmc: Course = course("LCMC")
+    val vir: Teacher = teacher("Viroli", Cons(pps, Nil()))
+    val ric: Teacher = teacher("Ricci", Cons(pcd, Nil()))
+    val brav: Teacher = teacher("Bravetti", Cons(lcmc, Nil()))
     
+    @Test def testCreateCourse() =
+        assertEquals("PPS", pps)
+
 
     
 }
