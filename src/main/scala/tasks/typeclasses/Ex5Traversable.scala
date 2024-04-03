cc5c9f96c3ea6e1a06bdea90ded47d76bf3dcef2
Tommaso <tommaso.brini@studio.unibo.it>
2024-04-03 17:43:40 +0200

    solved ex5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,9 @@
 package u04lab
 import u03.Sequences.* 
 import Sequence.*
+import u03.Optionals.Optional
+import Optional.*
+import u04lab.Ex5Traversable.logAll
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -18,9 +21,30 @@ import Sequence.*
 object Ex5Traversable:
 
   def log[A](a: A): Unit = println("The next element is: "+a)
+  
+  trait Traversable[T[_]]:
+    def logAll[A](t: T[A]): Unit
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+  def logAll[B, A[_]: Traversable](t: A[B]): Unit = summon[Traversable[A]].logAll(t)
+
+  given Traversable[Optional] with
+    def logAll[A](o: Optional[A]): Unit = o match
+      case Just(a) => log(a)
+      case _ => 
+
+  given Traversable[Sequence] with
+    def logAll[A](t: Sequence[A]): Unit = t match
+      case Cons(head, tail) => log(head); logAll(tail)
+      case _ => 
+      
+
+@main def testTraversable =
+  val seq = Cons(10, Cons(20, Cons(30, Nil())))
+  val opt = Just(10)
+
+  logAll(seq)
+  logAll(opt)
+
+  
 
   
